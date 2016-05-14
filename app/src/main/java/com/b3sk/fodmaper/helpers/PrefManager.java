321e773b9eef817e694eb8770e2f16be414f133e
Joopkins <jobndingdier@gmail.com>
2016-05-14 14:26:30 -0400

    feat: Add welcome screen on first launch

---
+++ app/src/main/java/com/b3sk/fodmaper/helpers/PrefManager.java
@@ -0,0 +1,36 @@
+package com.b3sk.fodmaper.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Joopkins on 5/13/16.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
