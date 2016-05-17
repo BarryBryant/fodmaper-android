6983c14e2c3a0459709559f46830db81417e8a04
Joopkins <jobndingdier@gmail.com>
2016-05-17 17:39:34 -0400

    tests: Expand food tests

--- app/src/test/java/com/b3sk/fodmaper/FoodTest.java
+++ app/src/test/java/com/b3sk/fodmaper/FoodTest.java
@@ -5,25 +5,44 @@ import com.b3sk.fodmaper.model.Food;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import static junit.framework.Assert.*;
 
 /**
  * Created by Joopkins on 5/14/16.
  */
+
 public class FoodTest {
 
-    Food food;
 
-    @Before
-    public void setup() {
-        food = new Food("Food", 1, 1, 0, 0, 0);
-    }
 
 
     @Test
     public void verifyGetFoodInfo() {
-        assertEquals(food.getInfo(), "Contains: Oligosaccharides ");
+        Food foodOne = new Food("Food", 0, 0, 0, 0, 0);
+        assertEquals("FODMAP Free", foodOne.getInfo());
+
+        Food foodTwo = new Food("Food", 1, 0, 1, 0, 0);
+        assertEquals("Contains: Disaccharides ", foodTwo.getInfo());
+
+        Food foodThree = new Food("Food", 1, 0, 0, 1, 0);
+        assertEquals("Contains: Monosaccharides ", foodThree.getInfo());
+
+        Food foodFour = new Food("Food", 1, 0, 0, 0, 1);
+        assertEquals("Contains: Polyols ", foodFour.getInfo());
+
+        Food foodFive = new Food("Food", 1, 1, 1, 0, 0);
+        assertEquals("Contains: Oligosaccharides Disaccharides ", foodFive.getInfo());
+
+        Food foodSix = new Food("Food", 1, 1, 0, 0, 0);
+        assertEquals("Contains: Oligosaccharides ", foodSix.getInfo());
     }
 
 }
