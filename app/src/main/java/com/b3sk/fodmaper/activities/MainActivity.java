5143b84b26758d37da60e20ba097188def5ff287
Joopkins <jobndingdier@gmail.com>
2016-05-18 20:14:47 -0400

    chore: Add signing config for udacity
    google-services.json had important data removed for safe upload

--- app/src/main/java/com/b3sk/fodmaper/activities/MainActivity.java
+++ app/src/main/java/com/b3sk/fodmaper/activities/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
         tracker = application.getDefaultTracker();
 
     }
-    
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
